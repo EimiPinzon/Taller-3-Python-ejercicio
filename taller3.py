c635deba8cae8325b5fff70b73d128067684481b
Eimi Pinzón <62155544+EimiPinzon@users.noreply.github.com>
2020-08-20 20:50:49 -0500

    Add files via upload

---
+++ taller3.py
@@ -0,0 +1,17 @@
+#Ejercicio 2
+lista1=("lunes", ",martes", "pan", "miercoles", "azucar")
+lista2=("jueves", "martes", "sabado", "domingo", "computador", "pan")
+
+print(lista1[1:3])
+
+l1=("lunes", "miercoles", "azucar")
+print(l1)
+
+l2=("jueves", "sabado", "domingo", "computador")
+print(l2)
+
+print(lista1 + lista2)
+
+
+
+
